93660acd717c8fd516502dde9c2b47217f72c3c5
Henrique Marques <88845710+henrigm4626@users.noreply.github.com>
2023-08-12 18:53:44 -0300

    Adding CODE

---
+++ 3-Potenciometro/LED_RGB_potenciometro.ino
@@ -0,0 +1,28 @@
+const int A = 6;
+const int B = 4;
+const int C = 2;
+int sensorValue = 0;
+
+void setup(){
+    pinMode(A, OUTPUT);
+    pinMode(B, OUTPUT);
+    pinMode(C, OUTPUT);
+    pinMode(A0, INPUT);
+}
+
+void loop(){
+  sensorValue = analogRead (A0);
+    digitalWrite(A, HIGH);
+    digitalWrite(B, LOW);
+    digitalWrite(C, LOW);
+  delay (sensorValue);
+    digitalWrite(A, LOW);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, LOW);
+  delay (sensorValue);
+    digitalWrite(A, LOW);
+    digitalWrite(B, LOW);
+    digitalWrite(C, HIGH);
+  delay (sensorValue);
+         
+}
